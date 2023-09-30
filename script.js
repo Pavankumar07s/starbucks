6c589a8a76b73a44adec54f79c017434ceb2e515
Pavankumar07s <136047863+Pavankumar07s@users.noreply.github.com>
2023-09-30 12:35:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.Starbucks').src = anything;
+}
+
+function changecirclecolor(color){
+    const circle=document.querySelector('.main #circle');
+    circle.style.background= color;
+}
